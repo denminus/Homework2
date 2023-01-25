2e48f9653b6bde1731bd915515f7c9816cf55939
den_minus <den.minus@mail.ru>
2023-01-25 20:36:38 +0300

    определение выходных

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("введите номер дня недели ");
+int value = int.Parse(Console.ReadLine()!);
+if (value>=1 && value<=7) 
+{
+    
+    if (value==6 || value==7)
+{
+    Console.WriteLine("этот номер дня недели соответствует выходному");
+}
+else Console.WriteLine("этот номер дня недели не соответствует выходному");
+}
+else 
+{
+    Console.WriteLine("это число не может быть номером дня недели");
+}
