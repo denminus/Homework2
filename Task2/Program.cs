99ce20d6b08af89c0cf94471c98f12e7a8814430
den_minus <den.minus@mail.ru>
2023-01-25 20:47:31 +0300

    определение квадрата по числам

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.Write("Введите целое большее число ");
+int bigvalue = int.Parse(Console.ReadLine()!);
+Console.Write("Введите меньшее число ");
+int smallvalue = int.Parse(Console.ReadLine()!);
+int osd = bigvalue / smallvalue;
+if (osd==smallvalue)
+{
+    Console.Write("Большее число является квадратом меньшего");
+}
+else 
+{
+    Console.Write("Большее число  не является квадратом меньшего");
+}
