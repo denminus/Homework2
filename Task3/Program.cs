aafd9cd2e3b5684dedde7da4436f7622718e4634
den_minus <den.minus@mail.ru>
2023-01-25 21:08:21 +0300

    Четверти координат

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.Write("Введите номер четверти координат ");
+int value = int.Parse(Console.ReadLine()!);
+if (value>=1 && value<=4)
+{
+    if (value==1) Console.WriteLine("x,y имеют положительные значения");
+    if (value==2) Console.WriteLine("x имеет отрицательное значение, y имеет положительное значение");
+    if (value==3) Console.WriteLine("x,y имеют отрицательные значения");
+    if (value==4) Console.WriteLine("x имеет положительное значение, y имеет отрицательное значение");
+
+    
+}
+else Console.WriteLine("Число не является номером четверти координат");
+
+
+
+
+
+
+
